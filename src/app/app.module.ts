b103b723849dd4c4845d28e4a1f9b7bdd37d9200
209 <209@academicos.cesde.edu.co>
2022-06-04 17:10:22 -0500

    se crea front de api hotel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
